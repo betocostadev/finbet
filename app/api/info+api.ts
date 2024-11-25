b3077731ca5062a604e812c3516e68c4d6a0ef97
betocostadev <robertomarx@gmail.com>
2024-11-25 18:40:39 -0300

    Feat: Adding api routes for crypto

---
+++ app/api/info+api.ts
@@ -0,0 +1,29 @@
+// export async function GET(request: Request) {
+//   return Response.json({ hello: 'world' })
+// }
+
+const API_KEY = process.env.CRYPTO_API_KEY
+
+// You can test at localhost:8081/api/info?id=1
+export async function GET(request: Request) {
+  const url = new URL(request.url)
+  const ids = url.searchParams.get('id')
+  console.log('ids are: ', ids)
+  if (!API_KEY) {
+    throw new Error('API key is missing')
+  }
+  const response = await fetch(
+    `https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${ids}`,
+    {
+      headers: {
+        'X-CMC_PRO_API_KEY': API_KEY,
+      },
+    }
+  )
+
+  const res = await response.json()
+
+  return new Response(JSON.stringify(res.data), {
+    headers: { 'Content-Type': 'application/json' },
+  })
+}
---
+++ app/api/listings+api.ts
@@ -0,0 +1,26 @@
+const API_KEY = process.env.CRYPTO_API_KEY
+
+// You can test at http://localhost:8081/api/listings
+export async function GET(request: Request) {
+  const url = new URL(request.url)
+  const limit = url.searchParams.get('limit') || 5
+
+  console.log('limits are: ', limit)
+  if (!API_KEY) {
+    throw new Error('API key is missing')
+  }
+  const response = await fetch(
+    `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=${limit}&convert=BRL`,
+    {
+      headers: {
+        'X-CMC_PRO_API_KEY': API_KEY,
+      },
+    }
+  )
+
+  const res = await response.json()
+
+  return new Response(JSON.stringify(res.data), {
+    headers: { 'Content-Type': 'application/json' },
+  })
+}
